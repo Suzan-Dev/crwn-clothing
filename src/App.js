977602553ab937daf530d04d2a3a2f23f3947e89
Sujan <everestgaming3@gmail.com>
2020-09-10 13:55:32 +0545

    homepage finished with smaller components and styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 // Components
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div>
+    <>
       <HomePage />
-    </div>
+    </>
   );
 }
 
