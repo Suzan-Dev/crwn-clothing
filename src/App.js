9a8d42c03a938c9633ead708c71c908b348b5250
Sujan <everestgaming3@gmail.com>
2020-09-11 14:35:11 +0545

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 // Components
 import HomePage from './pages/homepage/homepage.component';
 
+function HatsPage(props) {
+  // console.log(props);
+  return (
+    <>
+      <h1>Hats Page</h1>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </>
   );
 }
