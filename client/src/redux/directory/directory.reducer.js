70fdd9d5a1b5a123f4ea7f88eec42e4597f60b5d
Sujan Tamang <everestgaming3@gmail.com>
2021-06-13 08:53:20 +0545

    FEATURE: Updated homepage images

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,37 @@ export const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl:
+        'https://res.cloudinary.com/cloudimagex/image/upload/v1623552823/Crwn-Clothing/Homepage/hats_n8dibu.jpg',
       id: 1,
       linkUrl: 'shop/hats',
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl:
+        'https://res.cloudinary.com/cloudimagex/image/upload/v1623552831/Crwn-Clothing/Homepage/jackets_xi53xp.jpg',
       id: 2,
       linkUrl: 'shop/jackets',
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl:
+        'https://res.cloudinary.com/cloudimagex/image/upload/v1623553049/Crwn-Clothing/Homepage/sneakers_azozgi.jpg',
       id: 3,
       linkUrl: 'shop/sneakers',
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl:
+        'https://res.cloudinary.com/cloudimagex/image/upload/v1623552841/Crwn-Clothing/Homepage/womens_u1x8ee.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens',
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl:
+        'https://res.cloudinary.com/cloudimagex/image/upload/v1623553401/Crwn-Clothing/Homepage/mens_okoivq.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens',
